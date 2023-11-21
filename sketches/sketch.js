f0a9d072c81460fa92b988b108ad341d5b665724
Ada-Fun31 <95581380+Ada-Fun31@users.noreply.github.com>
2023-11-21 09:57:44 +0800

    P5downloaded

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -1,13 +1,21 @@
 const canvasSketch = require('canvas-sketch');
 
+// import p5 library
+const p5 = require("p5");
+new p5();
+
+// tell canvas we are using p5 library now
+// since canvas can use many different libraries
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  p5: true,
+  dimensions: [400, 400],
 };
 
+// use p5 syntax 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+  return () => {
+    background(0);
+    ellipse(200,200,50);
   };
 };
 
